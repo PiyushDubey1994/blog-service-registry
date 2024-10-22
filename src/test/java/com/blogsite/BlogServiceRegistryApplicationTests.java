8847cbb27d7bac780c79d7e401528fad5ebda32a
PiyusDubey1994 <Er_dubeypiyush@outlook.com>
2024-10-22 09:35:51 +0530

    initial commit
    initial commit

---
+++ src/test/java/com/blogsite/BlogServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
